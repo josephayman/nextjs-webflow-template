dbf274033e7156aa4e58cc2a32f4d22bb05c161a
Joseph Ayman <josephayman@mail.com>
2023-11-26 05:51:43 +0100

    Refactor styleguide page and add headings and paragraphs

--- src/app/styleguide/page.tsx
+++ src/app/styleguide/page.tsx
@@ -1,4 +1,5 @@
-import StyleguideHeader from "../ui/headers/styleguide-header";
+import Container from "@/app/ui/container";
+import StyleguideHeader from "@/app/ui/headers/styleguide-header";
 import HeadingJumbo from "@/app/ui/headings/heading-jumbo";
 
 export default function Home() {
@@ -6,10 +7,21 @@ export default function Home() {
     <>
       <StyleguideHeader />
 
-      <p>HEADINGS</p>
-      <HeadingJumbo>Heading Jumbo</HeadingJumbo>
-      <HeadingJumbo size="small">Heading Jumbo</HeadingJumbo>
-      <HeadingJumbo size="tiny">Heading Jumbo</HeadingJumbo>
+      <Container>
+        <p>HEADINGS</p>
+        <HeadingJumbo>Heading Jumbo</HeadingJumbo>
+        <HeadingJumbo size="small">Heading Jumbo</HeadingJumbo>
+        <HeadingJumbo size="tiny">Heading Jumbo</HeadingJumbo>
+
+        <h1>Heading 1</h1>
+        <h2>Heading 2</h2>
+        <h3>Heading 3</h3>
+        <h4>Heading 4</h4>
+        <h5>Heading 5</h5>
+        <h6>Heading 6</h6>
+
+        <p>PARAGRAPHS</p>
+      </Container>
     </>
   );
 }
