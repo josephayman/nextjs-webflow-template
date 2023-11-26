e46ff9313d7723fdb2cf89ab4ab5e9bc85f91a21
Joseph Ayman <josephayman@mail.com>
2023-11-26 04:01:39 +0100

    Add and remove logo files

--- src/app/ui/logo.tsx
+++ src/app/ui/logo.tsx
@@ -1,13 +1,17 @@
 import React from 'react'
 import Link from 'next/link'
+import Image from 'next/image'
 
 export default function Logo() {
   return (
     <Link href="/">
       <div className="flex items-center flex-shrink-0 mr-6">
-        <span className="font-semibold text-xl tracking-tight">
-          Business
-        </span>
+        <Image
+            src="/business-logo.png"
+            alt="Business"
+            width={108}
+            height={26}
+            />
       </div>
     </Link>
   )
