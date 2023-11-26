24368e6ba9f4c9076304303f9a7e1a7c37686cb6
Joseph Ayman <josephayman@mail.com>
2023-11-26 05:51:38 +0100

    Add Container component and update background color in StyleguideHeader

---
+++ src/app/ui/container.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+type ContainerProps = {
+    children: React.ReactNode
+    }
+
+export default function Container({ children }: ContainerProps) {
+  return (
+    <div className='mx-auto w-full max-w-6xl'>
+        {children}
+    </div>
+  )
+}
--- src/app/ui/headers/styleguide-header.tsx
+++ src/app/ui/headers/styleguide-header.tsx
@@ -3,7 +3,7 @@ import HeadingJumbo from '@/app/ui/headings/heading-jumbo'
 
 export default function StyleguideHeader() {
   return (
-    <div className='p-8 flex flex-col items-center justify-center text-center bg-black'>
+    <div className='p-8 flex flex-col items-center justify-center text-center bg-dark'>
         <HeadingJumbo color='white'>Styleguide</HeadingJumbo>
     </div>
   )
