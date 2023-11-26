d45f7ff4be804f58b8fcc0cc1da1ecfaeec7d55f
Joseph Ayman <josephayman@mail.com>
2023-11-26 14:37:59 +0100

    Add Divider component to styleguide page

--- src/app/styleguide/page.tsx
+++ src/app/styleguide/page.tsx
@@ -1,6 +1,7 @@
 import Container from "@/app/ui/container";
 import StyleguideHeader from "@/app/ui/headers/styleguide-header";
 import HeadingJumbo from "@/app/ui/headings/heading-jumbo";
+import Divider from "../ui/divider";
 
 export default function Home() {
   return (
@@ -19,7 +20,7 @@ export default function Home() {
         <h4>Heading 4</h4>
         <h5>Heading 5</h5>
         <h6>Heading 6</h6>
-
+        <Divider />
         <p>PARAGRAPHS</p>
       </Container>
     </>
---
+++ src/app/ui/divider.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Divider() {
+  return (
+    <div
+    className='w-full h-px bg-gray-200 my-8'
+    ></div>
+  )
+}
