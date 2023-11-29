6c38ba280b9de9b06f9f54d3a133b4382c4ce28d
Joseph Ayman <josephayman@mail.com>
2023-11-29 20:58:26 +0100

    Refactor HeadingJumbo component

--- src/app/ui/typography/heading-jumbo.tsx
+++ src/app/ui/typography/heading-jumbo.tsx
@@ -7,14 +7,21 @@ type HeadingJumboProps = {
   size?: "tiny" | "small" | "normal";
 };
 
-export default function HeadingJumbo({ children, color="black", size="normal" }: HeadingJumboProps) {
+export default function HeadingJumbo({
+  children,
+  color = "black",
+  size = "normal",
+}: HeadingJumboProps) {
   return (
-    <div className= {clsx(
-        size === "normal" && "text-jumbo leading-jumbo capitalize my-10px",
-        size === "small" && "text-jumbo-md leading-jumbo-md capitalize mt-10px mb-15px",
-        size === "tiny" && "text-jumbo-sm leading-jumbo-sm uppercase my-10px font-medium",
+    <div
+      className={clsx(
+        size === "normal" && "text-jumbo leading-jumbo  my-10px",
+        size === "small" && "text-jumbo-md leading-jumbo-md  mt-10px mb-15px",
+        size === "tiny" &&
+          "text-jumbo-sm leading-jumbo-sm uppercase my-10px font-medium",
         color === "white" && "text-white"
-        )}>
+      )}
+    >
       {children}
     </div>
   );
